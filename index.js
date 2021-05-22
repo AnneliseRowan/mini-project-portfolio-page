cbbe5725ce8a0dab2535c4136e769854fa66578e
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-05-22 14:18:13 -0400

    got it to work and generate html page

--- index.js
+++ index.js
@@ -1,56 +1,66 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-const generateHTML = (answers) =>
-`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <header>
-        <h1>${}</h1>
-        <h2>${}</h2>
-        <h3>${}</h3>
-        <h3>${}</h3>
-    </header>
-</body>
-</html>`; 
+const generateHTML = (answers) => 
+    `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <title>Document</title>
+    </head>
+    <body>
+        <header class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4>My name is ${answers.name}</h1>
+                <p class="lead">I am from ${answers.address}</p>
+                <h3> Example heading <span class="badge badge-secondary"> Contact Me</span></h3>
+    
+                <ul class="list-group">
+                    <li class="list-group-item">My github username is ${answers.linkedIn}</li>
+                    <li class="list-group-item">My github username is ${answers.github}</li>
+                </ul>
+    
+            </div>
+        </header>
+    </body>
+    </html>`; 
+     
 
-const promptUser = () => {
+inquirer
+    .prompt([
     {
-        type: 'input',
-        message: 'What is your Name?',
-        name: 'username',
+      type: 'input',
+      message: 'What is your Name?',
+      name: 'name',
     },
     {
-        type: 'input',
-        message: 'What is your street address?',
-        name: 'address',
+      type: 'input',
+      message: 'What is your street address?',
+      name: 'address',
     },
     {
-        type: 'input',
-        message: 'What is your LinkedIn profile?',
-        name: 'linkedIn',
+      type: 'input',
+      message: 'What is your LinkedIn profile?',
+      name: 'linkedIn',
     },
     {
-        type: 'input',
-        message: 'What is your Github username?',
-        name: 'Github',
+      type: 'input',
+      message: 'What is your Github username?',
+      name: 'github',
     },
-}
+  ]) 
+  .then((answers) => {
+    const htmlPageContent = generateHTML(answers);
 
-const init = () => {
-    promptUser()
-    .then((response) => writeFileAsync("index.html", gernateHTML(response)))
-    .then(() => console.log("successful"))
-    .catch((err) => console.log(err))
-}
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+}); 
 
-init(); 
-  
+
+
 
 
